a56ab74bf8ceaaf92c51a8193faaa73465eea074
JonLittle <jon@themodernlogicgroup.com>
2021-02-15 18:03:08 -0800

    updates
    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import { InstagramMedia } from 'react-instagram-media'
 Amplify.configure(aws_exports);
+
 
 class App extends Component {
   render() {
@@ -13,12 +15,224 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Music is why you are here..
+           
           </p>
-          
-          <p>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/obzqga8BnSo?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </p>
+          <InstagramMedia
+  uri="https://www.instagram.com/p/CLGPctRg-TB/"
+
+  renderItem={
+    ({ display_url, video_url, type, caption }) => {
+      if (type === 'video') {
+        return (
+          <video poster={display_url} controls>
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )
+      }
+
+      return (
+        <img
+          src={display_url}
+          alt={caption}
+        />
+      )
+    }
+  }
+
+  renderMediaList={children => (
+    <div className="swiper">
+      {children}
+    </div>
+  )}
+
+  renderError={() => (
+    <div>I have failed to parse it</div>
+  )}
+
+  renderLoading={() => (
+    <div>Loading</div>
+  )}
+/>
+<InstagramMedia
+  uri="https://www.instagram.com/p/CLGQ9KbgTG5/"
+
+  renderItem={
+    ({ display_url, video_url, type, caption }) => {
+      if (type === 'video') {
+        return (
+          <video poster={display_url} controls>
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )
+      }
+
+      return (
+        <img
+          src={display_url}
+          alt={caption}
+        />
+      )
+    }
+  }
+
+  renderMediaList={children => (
+    <div className="swiper">
+      {children}
+    </div>
+  )}
+
+  renderError={() => (
+    <div>I have failed to parse it</div>
+  )}
+
+  renderLoading={() => (
+    <div>Loading</div>
+  )}
+/>
+<InstagramMedia
+  uri="https://www.instagram.com/p/CLGPUFEgRMr/"
+
+  renderItem={
+    ({ display_url, video_url, type, caption }) => {
+      if (type === 'video') {
+        return (
+          <video poster={display_url} controls>
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )
+      }
+
+      return (
+        <img
+          src={display_url}
+          alt={caption}
+        />
+      )
+    }
+  }
+
+  renderMediaList={children => (
+    <div className="swiper">
+      {children}
+    </div>
+  )}
+
+  renderError={() => (
+    <div>I have failed to parse it</div>
+  )}
+
+  renderLoading={() => (
+    <div>Loading</div>
+  )}
+/>
+<InstagramMedia
+  uri="https://www.instagram.com/p/CLGPAPDgQ5p/"
+
+  renderItem={
+    ({ display_url, video_url, type, caption }) => {
+      if (type === 'video') {
+        return (
+          <video poster={display_url} controls>
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )
+      }
+
+      return (
+        <img
+          src={display_url}
+          alt={caption}
+        />
+      )
+    }
+  }
+
+  renderMediaList={children => (
+    <div className="swiper">
+      {children}
+    </div>
+  )}
+
+  renderError={() => (
+    <div>I have failed to parse it</div>
+  )}
+
+  renderLoading={() => (
+    <div>Loading</div>
+  )}
+/>
+<InstagramMedia
+  uri="https://www.instagram.com/p/CKIon8rg5FY/"
+
+  renderItem={
+    ({ display_url, video_url, type, caption }) => {
+      if (type === 'video') {
+        return (
+          <video poster={display_url} controls>
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )
+      }
+
+      return (
+        <img
+          src={display_url}
+          alt={caption}
+        />
+      )
+    }
+  }
+
+  renderMediaList={children => (
+    <div className="swiper">
+      {children}
+    </div>
+  )}
+
+  renderError={() => (
+    <div>I have failed to parse it</div>
+  )}
+
+  renderLoading={() => (
+    <div>Loading</div>
+  )}
+/>
+<InstagramMedia
+  uri="https://www.instagram.com/p/CLUvywFgRIA/"
+
+  renderItem={
+    ({ display_url, video_url, type, caption }) => {
+      if (type === 'video') {
+        return (
+          <video poster={display_url} controls>
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )
+      }
+
+      return (
+        <img
+          src={display_url}
+          alt={caption}
+        />
+      )
+    }
+  }
+
+  renderMediaList={children => (
+    <div className="swiper">
+      {children}
+    </div>
+  )}
+
+  renderError={() => (
+    <div>I have failed to parse it</div>
+  )}
+
+  renderLoading={() => (
+    <div>Loading</div>
+  )}
+/>
         </header>
       </div>
     );
